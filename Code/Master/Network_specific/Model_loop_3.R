6c6b8a6403ef0bcf9cb3c93af6a56d365cd24173
coconn41 <collinoconnor3@gmail.com>
2025-02-13 21:38:50 -0500

    Alter code to add debugging save every 10,000 timesteps.

--- Code/Master/Network_specific/Model_loop_3.R
+++ Code/Master/Network_specific/Model_loop_3.R
@@ -874,6 +874,23 @@ for(i in 1:go_timesteps){
   if(i%%100==0){print(paste0("timestep ", i, ", day ",day,", year ", year," in network ",net_select))
     # save.image(file = paste0(getwd(),"/Debugging/net_6_timestep_",i,".RData"))
   }
+  if(i%%10000==0){
+    write.csv(deer_agents,paste0(getwd(),"/Debugging/Network_",net_select,"/deer_debug_df_",
+                                 i,"_.csv"))
+    write.csv(mouse_agents,paste0(getwd(),"/Debugging/Network_",net_select,"/mouse_debug_df_",
+                                  i,"_.csv"))
+    write.csv(tick_agents,paste0(getwd(),"/Debugging/Network_",net_select,"/tick_debug_df_",
+                                 i,"_.csv"))
+    write.csv(deer_data2,paste0(getwd(),"/Debugging/Network_",net_select,"/Deer_results_debug_",
+                                net_select,"_",Sys.Date(),"_",substring(Sys.time(),12,16),
+                                "_.csv"))
+    write.csv(mouse_data2,paste0(getwd(),"/Debugging/Network_",net_select,"/Mouse_results_debug_",
+                                 net_select,"_",Sys.Date(),"_",substring(Sys.time(),12,16),
+                                 "_.csv"))
+    write.csv(tick_data2,paste0(getwd(),"/Debugging/Network_",net_select,"/Tick_results_debug_",
+                                net_select,"_",Sys.Date(),"_",substring(Sys.time(),12,16),
+                                "_.csv"))
+  }
 }
 # end_time = Sys.time()
 # end_time - start_time
