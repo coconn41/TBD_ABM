d21530665e892506cff6fc7624c9db5f3fc0c3cc
coconn41 <collinoconnor3@gmail.com>
2026-07-20 15:15:25 -0400

    Add load and saves

---
+++ HPCC_scripts/Harvard/verbose/load_and_save.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "starting load_and_save.sh"
+
+# Submit the pipeline as a background process with ./run.sh
+# module load R # Uncomment if R is an environment module.
+
+Rscript ./R/TBD_ABM/Code/Post_sim_analysis/Simplified_analysis.R
+
+# Change the nice level above as appropriate
+# for your situation and system.
+
+# Removing .RData is recommended.
+rm -f .RData
+
+echo "ending load_and_save.sh"
